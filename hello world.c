95c7b7822b8f71954ccbeeb6ddb01fdb294e4f0f
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-08-23 22:55:56 -0300

    Add files via upload

---
+++ hello world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h.>
+
+int main(){
+	
+	//codigo para imprimir o que está em parenteses//
+	
+	printf("Hello, world\n");
+	
+	//forma de colocar no windows o codigo//
+	
+	system("pause");
+}
